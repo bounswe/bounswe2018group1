204bebff17db8bfdb7940f487150b28ab3e075c0
ailerle <akinilerle@gmail.com>
2018-04-02 23:12:45 +0300

    webserver initial commit

---
+++ twitter project/webserver/src/main/java/com/cmpe352_group1/twitter_project/TwitterProjectApplication.java
@@ -0,0 +1,12 @@
+package com.cmpe352_group1.twitter_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterProjectApplication.class, args);
+	}
+}
