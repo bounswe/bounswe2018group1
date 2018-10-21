a84489d6f9faec60fb9b27e7775a1a8b016e44c2
Emre Bilgili <emre---bilgili@hotmail.com>
2018-10-22 02:08:06 +0300

    toolbar update

--- android/app/src/main/java/bounswe2018group1/cmpe451/MainActivity.java
+++ android/app/src/main/java/bounswe2018group1/cmpe451/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    // TODO Map tab may not be implemented and removed
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
