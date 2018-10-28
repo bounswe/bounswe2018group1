8a97932099a0005504e3bceb662c353f27f69b00
denizetkar <denizetkar@gmail.com>
2018-10-29 00:16:07 +0300

    added listview to the feed and some dummy memories

--- android/app/src/main/java/bounswe2018group1/cmpe451/fragments/FeedFragment.java
+++ android/app/src/main/java/bounswe2018group1/cmpe451/fragments/FeedFragment.java
@@ -6,8 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import bounswe2018group1.cmpe451.R;
+import bounswe2018group1.cmpe451.helpers.MemoryAdapter;
 
 
 /**
@@ -15,6 +25,8 @@ import bounswe2018group1.cmpe451.R;
  */
 public class FeedFragment extends Fragment {
 
+    private JsonArray dataSource;
+    private ListView listView;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -25,7 +37,21 @@ public class FeedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        listView = rootView.findViewById(R.id.listView);
+
+        InputStream is = getResources().openRawResource(R.raw.memory_json_file);
+        try {
+            dataSource = new JsonParser().parse(
+                    new BufferedReader(new InputStreamReader(is, "UTF-8"))
+            ).getAsJsonArray();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        MemoryAdapter adapter = new MemoryAdapter(rootView.getContext(), R.layout.memory_row, dataSource);
+        listView.setAdapter(adapter);
+        return rootView;
     }
 
 }
