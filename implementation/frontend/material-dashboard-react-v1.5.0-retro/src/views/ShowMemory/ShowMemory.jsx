ec6923a4a15c753870dd10a9a050bc06f73d7b56
Ece Ata <eceatata@hotmail.com>
2018-10-30 15:39:50 +0300

    sunum

--- implementation/frontend/material-dashboard-react-v1.5.0-retro/src/views/ShowMemory/ShowMemory.jsx
+++ implementation/frontend/material-dashboard-react-v1.5.0-retro/src/views/ShowMemory/ShowMemory.jsx
@@ -49,14 +49,17 @@ class ShowMemory extends React.Component {
                 <p className={classes.cardCategoryWhite}>
                   The best places of Denizli :)
                 </p>
+                <p className={classes.cardCategoryWhite}>
+                  Added by BuseEce20.
+                </p>
               </CardHeader>
               <CardBody>
                 <Table
                   tableHeaderColor="warning"
                   tableHead={["Story Name", "Start Date", "End Date", "Location", "Description"]}
                   tableData={[
-                    ["Çamlık Parkı", "2018.10.19", "2018.10.19", "Denizli, Turkey", "Denizli is a town with tree-lined..."],
-                    ["Pamukkale", "2018.10.23", "2018.10.23", "Denizli, Turkey", "Pamukkale, meaning cotton castle..."]
+                    ["Çamlık Parkı", "2018.10.19", "2018.10.19", "Denizli, Turkey", "We had a wonderful time..."],
+                    ["Pamukkale", "2018.10.23", "2018.10.23", "Denizli, Turkey", "This was our group gathering..."]
                   ]}
                 />
               </CardBody>
@@ -70,6 +73,9 @@ class ShowMemory extends React.Component {
                 <h4 className={classes.cardTitleWhite}>Edirne Tour</h4>
                 <p className={classes.cardCategoryWhite}>
                   The most beautiful places!
+                </p>
+                <p className={classes.cardCategoryWhite}>
+                  Added by BuseEce20.
                 </p>
               </CardHeader>
               <CardBody>
