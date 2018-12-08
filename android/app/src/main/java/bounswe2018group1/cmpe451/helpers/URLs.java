36bb191e166ed16c4cb712fd9c716484c91c78fc
Emre Bilgili <emre---bilgili@hotmail.com>
2018-12-08 22:45:42 +0300

    Server addresses have been updated to :8080

--- android/app/src/main/java/bounswe2018group1/cmpe451/helpers/URLs.java
+++ android/app/src/main/java/bounswe2018group1/cmpe451/helpers/URLs.java
@@ -2,18 +2,18 @@ package bounswe2018group1.cmpe451.helpers;
 
 public class URLs {
 
-    public static final String URL_ACTIVATE = "http://52.7.87.173:5000/activate";
-    public static final String URL_LOGIN = "http://52.7.87.173:5000/login";
-    public static final String URL_LOGOUT = "http://52.7.87.173:5000/logout";
-    public static final String URL_REGISTER = "http://52.7.87.173:5000/register";
+    public static final String URL_ACTIVATE = "http://52.7.87.173:8080/activate";
+    public static final String URL_LOGIN = "http://52.7.87.173:8080/login";
+    public static final String URL_LOGOUT = "http://52.7.87.173:8080/logout";
+    public static final String URL_REGISTER = "http://52.7.87.173:8080/register";
 
-    public static final String URL_MEMORY = "http://52.7.87.173:5000/memory";
-    public static final String URL_MEMORY_ALL = "http://52.7.87.173:5000/memory/all";
-    public static final String URL_MEMORY_USER = "http://52.7.87.173:5000/memory/user";
+    public static final String URL_MEMORY = "http://52.7.87.173:8080/memory";
+    public static final String URL_MEMORY_ALL = "http://52.7.87.173:8080/memory/all";
+    public static final String URL_MEMORY_USER = "http://52.7.87.173:8080/memory/user";
 
-    public static final String URL_USER = "http://52.7.87.173:5000/user";
-    public static final String URL_USER_ALL = "http://52.7.87.173:5000/user/all";
-    public static final String URL_USER_INFO = "http://52.7.87.173:5000/user/info";
+    public static final String URL_USER = "http://52.7.87.173:8080/user";
+    public static final String URL_USER_ALL = "http://52.7.87.173:8080/user/all";
+    public static final String URL_USER_INFO = "http://52.7.87.173:8080/user/info";
 
     public static String URL_USER_withID(String id) {
         return URL_USER + '/' + id;
