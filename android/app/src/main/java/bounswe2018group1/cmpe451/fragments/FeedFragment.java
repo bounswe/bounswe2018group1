eb99b2d586aa68b68dbad5e033e34ba2b1e493d1
denizetkar <denizetkar@gmail.com>
2018-12-11 21:41:34 +0300

    added pull down to refresh into memory feed page

--- android/app/src/main/java/bounswe2018group1/cmpe451/fragments/FeedFragment.java
+++ android/app/src/main/java/bounswe2018group1/cmpe451/fragments/FeedFragment.java
@@ -2,8 +2,10 @@ package bounswe2018group1.cmpe451.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,9 +28,10 @@ public class FeedFragment extends Fragment {
 
     private JsonArray dataSource = null;
     private Semaphore dataSourceUpdateLock = null;
+    private SwipeRefreshLayout swipeRefreshLayout = null;
     private ListView listView = null;
     private ClientAPI clientAPI = null;
-    private int pageSize = 4;
+    private int pageSize = 5;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -39,27 +42,78 @@ public class FeedFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
 
+        if (swipeRefreshLayout == null) swipeRefreshLayout = rootView.findViewById(R.id.swipeRefreshLayout);
         if (listView == null) listView = rootView.findViewById(R.id.listView);
         if(clientAPI == null) clientAPI = ClientAPI.getInstance(getContext());
 
         if (dataSource == null) dataSource = new JsonArray();
         if(dataSourceUpdateLock == null) dataSourceUpdateLock = new Semaphore(1);
-        final MemoryAdapter adapter = new MemoryAdapter(rootView.getContext(), R.layout.memory_row, dataSource);
+        MemoryAdapter adapter = new MemoryAdapter(rootView.getContext(), R.layout.memory_row, dataSource);
         listView.setAdapter(adapter);
 
         updateListView(adapter);
         implementScrollListener(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(getActivity(), MemoryViewActivity.class);
-                i.putExtra("memory", adapter.getItem(position).toString());
-                startActivity(i);
-            }
-        });
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayoutOnRefreshListener(adapter, rootView));
+        // Scheme colors for animation
+        swipeRefreshLayout.setColorSchemeColors(
+                getResources().getColor(android.R.color.holo_blue_bright),
+                getResources().getColor(android.R.color.holo_green_light),
+                getResources().getColor(android.R.color.holo_orange_light),
+                getResources().getColor(android.R.color.holo_red_light)
+        );
+
+        listView.setOnItemClickListener(new ListViewOnItemClickListener(adapter));
 
         return rootView;
+    }
+
+    class SwipeRefreshLayoutOnRefreshListener implements SwipeRefreshLayout.OnRefreshListener {
+
+        private MemoryAdapter memoryAdapter;
+        private View rootView;
+
+        SwipeRefreshLayoutOnRefreshListener(MemoryAdapter memoryAdapter, View rootView) {
+            this.memoryAdapter = memoryAdapter;
+            this.rootView = rootView;
+        }
+
+        @Override
+        public void onRefresh() {
+            try {
+                dataSourceUpdateLock.acquire();
+                dataSource = new JsonArray();
+                memoryAdapter = new MemoryAdapter(rootView.getContext(), R.layout.memory_row, dataSource);
+                listView.setAdapter(memoryAdapter);
+                memoryAdapter.notifyDataSetChanged();
+                dataSourceUpdateLock.release();
+                updateListView(memoryAdapter);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            new Handler().postDelayed(new Runnable() {
+                @Override public void run() {
+                    // Stop animation (This will be after 3 seconds)
+                    swipeRefreshLayout.setRefreshing(false);
+                }
+            }, 500); // Delay in millis
+        }
+    }
+
+    class ListViewOnItemClickListener implements AdapterView.OnItemClickListener {
+
+        private MemoryAdapter memoryAdapter;
+
+        ListViewOnItemClickListener(MemoryAdapter memoryAdapter) {
+            this.memoryAdapter = memoryAdapter;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Intent i = new Intent(getActivity(), MemoryViewActivity.class);
+            i.putExtra("memory", memoryAdapter.getItem(position).toString());
+            startActivity(i);
+        }
     }
 
     // Implement scroll listener
@@ -69,35 +123,22 @@ public class FeedFragment extends Fragment {
 
     private class MyOnScrollListener implements AbsListView.OnScrollListener {
 
-        private boolean userScrolled;
         private MemoryAdapter adapter;
 
         MyOnScrollListener(MemoryAdapter adapter) {
-            this.userScrolled = false;
             this.adapter = adapter;
         }
 
         @Override
         public void onScrollStateChanged(AbsListView view, int scrollState) {
-            // If scroll state is touch scroll then set userScrolled
-            // true
-            if (scrollState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
-                userScrolled = true;
+            if(scrollState == SCROLL_STATE_IDLE && listView.getLastVisiblePosition() ==
+                    dataSource.size() - 1){
+                updateListView(adapter);
             }
         }
 
         @Override
-        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-            // Now check if userScrolled is true and also check if
-            // the item is end then update list view and set
-            // userScrolled to false
-            if (userScrolled
-                    && firstVisibleItem + visibleItemCount == totalItemCount) {
-
-                userScrolled = false;
-                updateListView(adapter);
-            }
-        }
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {}
     }
 
     // Method for repopulating recycler view
