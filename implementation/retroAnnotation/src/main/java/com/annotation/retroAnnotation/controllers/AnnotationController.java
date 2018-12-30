1e3169dc9d354f3af3b147a55ce100c4ad2de692
Buse Ece Güldiken <buseecegs@gmail.com>
2018-12-30 15:03:18 +0300

    added controllers

---
+++ implementation/retroAnnotation/src/main/java/com/annotation/retroAnnotation/controllers/AnnotationController.java
@@ -0,0 +1,38 @@
+package com.annotation.retroAnnotation.controllers;
+
+import com.annotation.retroAnnotation.models.CreateAnnotationRequestBody;
+import com.annotation.retroAnnotation.models.CreateAnnotationResponseBody;
+import com.annotation.retroAnnotation.models.GetAnnotationResponseBody;
+import com.annotation.retroAnnotation.services.AnnotationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@CrossOrigin(origins = "*", allowCredentials ="true", allowedHeaders ="*",
+        methods = {RequestMethod.HEAD,RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE})
+
+@RestController
+public class AnnotationController {
+
+    @Autowired
+    AnnotationService annotationService;
+
+    @RequestMapping(value = "/annotation",method = RequestMethod.POST)
+    public CreateAnnotationResponseBody createAnnotation(@RequestBody CreateAnnotationRequestBody requestBody){
+        return annotationService.createAnnotation(requestBody);
+    }
+
+    @RequestMapping(value = "/annotation",method = RequestMethod.GET)
+    public GetAnnotationResponseBody getAnnotation(Long id) throws Exception {
+        return annotationService.getAnnotation(id);
+    }
+
+    @RequestMapping(value = "/annotation/item",method = RequestMethod.GET)
+    public List<GetAnnotationResponseBody> getAnnotationOfItem(Long itemId) {
+        return annotationService.getAnnotationsOfItem(itemId);
+    }
+
+
+}
