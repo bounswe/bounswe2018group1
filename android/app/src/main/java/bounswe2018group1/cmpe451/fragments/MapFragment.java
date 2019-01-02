44f5b76465273cd280b8573d17e87ca191ffc7a9
Emre Bilgili <emre---bilgili@hotmail.com>
2019-01-02 11:45:52 +0300

    Search UI added

--- android/app/src/main/java/bounswe2018group1/cmpe451/fragments/MapFragment.java
+++ android/app/src/main/java/bounswe2018group1/cmpe451/fragments/MapFragment.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import bounswe2018group1.cmpe451.MapsSearchActivity;
 import bounswe2018group1.cmpe451.R;
@@ -21,7 +24,11 @@ public class MapFragment extends Fragment {
 
     private static final int PICK_MAP_POINT = 999;
 
-    private Button mapSearch;
+    private Button searchMap;
+    private Button searchButton;
+    private TextView searchText;
+    private TextView searchTag;
+    private TextView searchLocation;
 
     public MapFragment() {
         // Required empty public constructor
@@ -32,13 +39,24 @@ public class MapFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
 
-        mapSearch = v.findViewById(R.id.mapSearch);
+        searchMap = v.findViewById(R.id.searchMap);
+        searchButton = v.findViewById(R.id.searchButton);
+        searchText = v.findViewById(R.id.searchText);
+        searchTag = v.findViewById(R.id.searchTag);
+        searchLocation = v.findViewById(R.id.searchLocation);
 
-        mapSearch.setOnClickListener(new View.OnClickListener() {
+        searchMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent pickPointIntent = new Intent(getActivity(), MapsSearchActivity.class);
                 startActivityForResult(pickPointIntent, PICK_MAP_POINT);
+            }
+        });
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
 
